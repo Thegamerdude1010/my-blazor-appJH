f01b859f96ca6378c8b90bda7bc7f2f055f32530
Jacob Holmes <jrholmes1480@eagle.fgcu.edu>
2021-09-29 18:06:15 -0400

    added comments to Todo.razor and Todoitem.cs

--- Client/TodoItem.cs
+++ Client/TodoItem.cs
@@ -1,5 +1,5 @@
 public class TodoItem
 {
-    public string Title { get; set; }
-    public bool IsDone { get; set; }
+    public string Title { get; set; } //this is the title for an individual todo
+    public bool IsDone { get; set; } //this reads true or false based on if a todo is checked as done
 }
