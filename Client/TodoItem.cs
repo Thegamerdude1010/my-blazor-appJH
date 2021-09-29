42ade752003e68e5b9f53aef3ff551e54929f6ca
Jacob Holmes <jrholmes1480@eagle.fgcu.edu>
2021-09-29 19:27:52 -0400

    added more comments to TodoItem.cs and Todo.razor

--- Client/TodoItem.cs
+++ Client/TodoItem.cs
@@ -1,4 +1,4 @@
-public class TodoItem
+public class TodoItem // This is the class for created todo items, it holds the title and whether it is done
 {
     public string Title { get; set; } //this is the title for an individual todo
     public bool IsDone { get; set; } //this reads true or false based on if a todo is checked as done
